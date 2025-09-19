514d1b14b33bb120bd905fe4dd9c620cca8bc914
Sathyavit <sathya.m2023@vitstudent.ac.in>
2025-09-19 14:40:06 +0530

    Create Q2.php

---
+++ Assignment 8/Q2.php
@@ -0,0 +1,11 @@
+<?php
+$color = array('white', 'green', 'red');
+
+echo implode(", ", $color) . ",";
+
+echo "<br><br>";
+
+echo "• $color[1]<br>";
+echo "• $color[2]<br>";
+echo "• $color[0]<br>";
+?>
